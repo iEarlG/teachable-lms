1c5a7764dacadcbfa3f566cb2a77b096c6aa7d52
Earl A. Villapaz <jasonburning16@gmail.com>
2023-09-30 21:07:44 +0800

    2: Sidebar (Navigations) Sidebar Item Layout

--- components/navigations/sidebar/SidebarItems.tsx
+++ components/navigations/sidebar/SidebarItems.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { usePathname, useRouter } from "next/navigation";
+
 import { LucideIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
 
 interface SidebarItemsProps {
     icon: LucideIcon;   
@@ -13,9 +17,38 @@ export const SidebarItems = ({
     label,
     href
 }: SidebarItemsProps) => {
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const isActive = (pathname === "/" && href === "/") || 
+        pathname === href || 
+        pathname?.startsWith(`${href}/`);
+
+    const onClick = () => {
+        router.push(href);
+    };
+    
     return (
-        <div>
-            SidebarItems
-        </div>
+        <button
+            onClick={onClick}
+            type="button"
+            className={cn("flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 hover:text-slate-600 hover:bg-sky-300/20",
+                isActive && "bg-sky-700 bg-slate-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+            )}
+        >
+            <div className="flex items-center gap-x-2 py-4">
+                <Icon 
+                    size={20} 
+                    className={cn("text-slate-500",
+                        isActive && "text-sky-700"
+                    )} 
+                />
+                <span>{label}</span>
+            </div>
+            <div className={cn("h-full ml-auto opacity-0 border-[1px] border-sky-700 transition-all",
+                isActive && "opacity-100"
+            )} 
+            />
+        </button>
     )
 };
