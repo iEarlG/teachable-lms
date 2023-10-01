fbb9482a51c4517e82f8aa6dace591115ed3ac04
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-01 18:07:43 +0800

    3: Navbar & Mobile Navbar Layout

---
+++ components/navigations/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import { MobileSidebar } from "@/components/navigations/navbar/MobileSidebar";
+
+export const Navbar = () => {
+    return (
+        <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+            <MobileSidebar />
+        </div>
+    )
+};
