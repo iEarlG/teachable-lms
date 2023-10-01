0e9ccc456cdd626dae3dc8c2158209436022b4dc
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-01 18:11:37 +0800

    3: Navbar & Mobile Navbar Layout partial DONE

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
+      This is route page
     </div>
   )
 };
--- components/navigations/navbar/Navbar.tsx
+++ components/navigations/navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import { MobileSidebar } from "@/components/navigations/navbar/MobileSidebar";
+import { NavbarRoutes } from "@/components/navigations/navbar/NavbarRoutes";
 
 export const Navbar = () => {
     return (
         <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
             <MobileSidebar />
+            <NavbarRoutes />
         </div>
     )
 };
