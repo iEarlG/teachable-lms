00b0376afb052db218dd3fecb4d93e4a1d97ceb7
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-03 19:13:13 +0800

    4: Teacher Mode Create Course Layout *Dynamic att this moment

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const CoursesPage = () => {
     return ( 
-        <div>
-            courses
+        <div className="p-6">
+            <Link href="/teacher/create">
+                <Button>
+                    Add new courses
+                </Button>
+            </Link>
         </div>
     );
 }
---
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const CreatePage = () => {
+    return ( 
+        <div>
+            This is the create Button
+        </div>
+    );
+}
+ 
+export default CreatePage;
