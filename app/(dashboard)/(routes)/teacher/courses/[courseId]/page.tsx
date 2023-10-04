cfd0c7d912199d66df1e9af9057165893f0deba4
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-04 18:09:47 +0800

    5: Course Setup process

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,8 +1,50 @@
 
-const CourseIdPage = ({ params }: {params: {courseId: string}}) => {
+import { auth } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({ 
+    params 
+}: {
+    params: {courseId: string}
+}) => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const course = await db.course.findUnique({
+        where: {
+            id: params.courseId
+        }
+    });
+
+    if (!course) {
+        return redirect("/");
+    }
+
+    const requiredFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId
+    ];
+
+    const totalFields = requiredFields.length;
+    const completedFields = requiredFields.filter(Boolean).length;
+    const percentComplete = `(${completedFields}/${totalFields})`;
+
     return ( 
-        <div>
-            Course ID: {params.courseId}
+        <div className="p-6">
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-y-2">
+                    <h1 className="text-2xl font-medium">Course Setup</h1>
+                    <span className="text-xs text-slate-700">Complete all the fields {percentComplete} to continue</span>
+                </div>
+            </div>
         </div>
     );
 }
