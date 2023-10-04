093f8d162511925fcb5da82ff7b342b6c89695ad
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-04 18:42:04 +0800

    5: Course Setup process UI

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,8 +1,12 @@
 
+import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs";
 
+import { LayoutDashboard } from "lucide-react";
+
 import { db } from "@/lib/db";
-import { redirect } from "next/navigation";
+
+import { IconBadge } from "@/components/ui/IconBadge";
 
 const CourseIdPage = async ({ 
     params 
@@ -43,6 +47,14 @@ const CourseIdPage = async ({
                 <div className="flex flex-col gap-y-2">
                     <h1 className="text-2xl font-medium">Course Setup</h1>
                     <span className="text-xs text-slate-700">Complete all the fields {percentComplete} to continue</span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={LayoutDashboard} size="md" />
+                        <h2 className="text-xl">Customize your course</h2>
+                    </div>
                 </div>
             </div>
         </div>
