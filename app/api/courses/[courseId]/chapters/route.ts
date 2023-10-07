3e146b039c65fac9c725b8c3f9420546c00e0adf
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-07 20:31:22 +0800

    12: Course Chapters Form Route API

---
+++ app/api/courses/[courseId]/chapters/route.ts
@@ -0,0 +1,53 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(
+    req: Request,
+    {params}: {params: {courseId: string}}
+) {
+    try {
+        const { userId } = auth();
+        const { title } = await req.json();
+
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const courseOwner = await db.course.findUnique({
+            where: {
+                id: params.courseId,
+                userId
+            }
+        });
+
+        if (!courseOwner) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const lastChapter = await db.chapter.findFirst({
+            where: {
+                courseId: params.courseId,
+            },
+            orderBy: {
+                position: "desc"
+            }
+        });
+
+        const newCreatedChapter = lastChapter ? lastChapter.position + 1 : 1;
+
+        const chapter = await db.chapter.create({
+            data: {
+                title,
+                courseId: params.courseId,
+                position: newCreatedChapter,
+            }
+        });
+
+        return NextResponse.json(chapter);
+
+    } catch (error) {
+        console.log(error, "Error while fetching course chapters [COURSE ID API]");
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
