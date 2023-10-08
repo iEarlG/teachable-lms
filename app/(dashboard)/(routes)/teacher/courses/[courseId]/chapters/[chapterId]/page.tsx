2700cbdc69f4ffc6df8877050603d215c95b843c
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-08 20:14:59 +0800

    13: Course Chapter Setup Layout

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -2,7 +2,8 @@ import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs";
 import { db } from "@/lib/db";
 import Link from "next/link";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { IconBadge } from "@/components/ui/IconBadge";
 
 const ChapterIdPage = async ({
     params,
@@ -56,6 +57,17 @@ const ChapterIdPage = async ({
                             <h1 className="text-2xl font-medium">Course Chapter Setup</h1>
                             <span className="text-xs text-slate-700">Complete all fields {chapterCompletion} to continue</span>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div className="space-y-4">
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={LayoutDashboard} size="md" />
+                            <h2 className="text-xl">Customize your Chapters</h2>
+                        </div>
+                        <p className="text-sm text-slate-700">Add a title, description and video to your chapter</p>
                     </div>
                 </div>
             </div>
