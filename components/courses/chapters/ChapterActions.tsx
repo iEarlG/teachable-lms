7d285b740cf427f023e54e71ab05f50e8452bc7c
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-10 19:54:58 +0800

    18: Course Chapter Actions Layout

--- components/courses/chapters/ChapterActions.tsx
+++ components/courses/chapters/ChapterActions.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import axios from "axios";
+
 import { Trash } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { ConfirmModal } from "@/components/modals/ConfirmModal";
 
 interface ChapterActionsProps {
     chapterId: string;
@@ -17,19 +23,38 @@ export const ChapterActions = ({
     isPublished,
     disabled,
 }: ChapterActionsProps) => {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false)
+    
+    const onDeleteChapter = async () => {
+        try {
+            setIsLoading(true);
+
+            await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+            toast.success("Chapter deleted successfully.");
+            router.refresh();
+            router.push(`/teacher/courses/${courseId}`)
+        } catch {
+            toast.error("Something went wrong. Please try again.")
+        } finally {
+            setIsLoading(false);
+        }
+    };
     return (
         <div className="flex items-center gap-x-2">
             <Button
                 variant="outline"
                 size="sm"
                 onClick={() => {}}
-                disabled={disabled}
+                disabled={disabled || isLoading}
             >
                 {isPublished ? "Unpublish" : "Publish"}
             </Button>
-            <Button size="sm">
-                <Trash className="h-4 w-4" />
-            </Button>
+            <ConfirmModal onConfirm={onDeleteChapter}>
+                <Button size="sm" disabled={isLoading}>
+                    <Trash className="h-4 w-4" />
+                </Button>
+            </ConfirmModal>
         </div>
     );
 };
---
+++ components/modals/ConfirmModal.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, 
+    AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "../ui/alert-dialog";
+
+interface ConfirmModalProps {
+    children: React.ReactNode;
+    onConfirm: () => void;
+};
+
+export const ConfirmModal = ({
+    children,
+    onConfirm
+}: ConfirmModalProps) => {
+
+
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                {children}
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Think before you click</AlertDialogTitle>
+                    <AlertDialogDescription>Are you sure you want to delete this chapter?</AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={onConfirm}>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+};
