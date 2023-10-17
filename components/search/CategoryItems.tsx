6b864667ebe87a9495edb86636b89b2244451f3c
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-17 18:46:09 +0800

    20: Categories

--- components/search/CategoryItems.tsx
+++ components/search/CategoryItems.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import qs from "query-string";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { IconType } from "react-icons";
+
+import { cn } from "@/lib/utils";
 
 interface CategoryItemsProps {
     label: string;
@@ -14,8 +17,35 @@ export const CategoryItems = ({
     icon: Icon,
     value
 }: CategoryItemsProps) => {
+    const pathname = usePathname();
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const currentCategory = searchParams.get("categoryId");
+    const currentTitle = searchParams.get("title");
+
+    const isSelected = currentCategory === value;
+
+    const onClick = () => { 
+        const url = qs.stringifyUrl({
+            url: pathname,      
+            query: {
+                title: currentTitle,
+                categoryId: isSelected ? null : value,
+            }
+        }, {skipNull: true, skipEmptyString: true});
+
+        router.push(url);
+    };
+
     return (
-        <button className={cn("flex items-center gap-x-1 py-2 px-3 text-sm border border-slate-200 rounded-full hover:border-green-700 transition")}>
+        <button 
+            type="button"
+            onClick={onClick}
+            className={cn("flex items-center gap-x-1 py-2 px-3 text-sm border border-slate-200 rounded-full hover:border-green-700 transition",
+                isSelected && "border-green-700 bg-green-200/20 text-neutral-800"
+            )}
+        >
             {Icon && <Icon size={20} />}
             <div className="truncate">
                 {label}
