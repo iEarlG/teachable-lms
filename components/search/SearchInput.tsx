8068f67d0470371078ad4aeade94aa408c137faa
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-17 19:32:40 +0800

    21: Search Query

--- components/search/SearchInput.tsx
+++ components/search/SearchInput.tsx
@@ -1,9 +1,37 @@
 "use client";
 
+import qs from "query-string";
+import { useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 import { Search } from "lucide-react";
+import { useDebounce } from "@/hooks/useDebounce";
+
 import { Input } from "@/components/ui/input";
 
 export const SearchInput = () => {
+    const [value, setValue] = useState("");
+    const debounceValue = useDebounce(value);
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const currentCategory = searchParams.get("categoryId");
+
+    useEffect(() => {
+        const url = qs.stringifyUrl({
+            url: pathname,
+            query: {
+                categoryId: currentCategory,
+                title: debounceValue,
+            }
+        }, { skipEmptyString: true, skipNull: true });
+
+        router.push(url);
+
+    }, [debounceValue, currentCategory, router, pathname]);
+    
+
     return (
         <div className="relative">
             <Search 
@@ -12,6 +40,8 @@ export const SearchInput = () => {
             <Input 
                 className="w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
                 placeholder="Search for courses"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
             />
         </div>
     )
