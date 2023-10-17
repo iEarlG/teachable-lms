8bcf4cbe0d95d0717c244bb5b3bc7a7eceeb5aa8
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-17 19:34:56 +0800

    21: Search Query

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -2,6 +2,7 @@
 import { db } from "@/lib/db";
 
 import { Categories } from "@/components/search/Categories";
+import { SearchInput } from "@/components/search/SearchInput";
 
 const SearchPage = async () => {
     const categories = await db.category.findMany({
@@ -10,12 +11,17 @@ const SearchPage = async () => {
         }
     });
 
-    return ( 
-        <div className="p-6">
-            <Categories 
-                items={categories}
-            /> 
-        </div>
+    return (
+        <>
+            <div className="px-6 pt-6 md:hidden md:mb-0 block">
+                <SearchInput />
+            </div>
+            <div className="p-6">
+                <Categories 
+                    items={categories}
+                /> 
+            </div>
+        </>
     );
 }
  
