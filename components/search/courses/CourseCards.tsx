f10bd66fe397fe6b41c2bdbc400e7a0b8655e042
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-18 21:17:12 +0800

    24: Course Card Grid UI

--- components/search/courses/CourseCards.tsx
+++ components/search/courses/CourseCards.tsx
@@ -1,6 +1,10 @@
+import Image from "next/image";
 import Link from "next/link";
 
+import { BookOpen } from "lucide-react";
 
+import { IconBadge } from "@/components/ui/IconBadge";
+import { formatPrice } from "@/lib/priceformat";
 interface CourseCardsProps {
     id: string;
     title: string;
@@ -22,8 +26,40 @@ export const CourseCards = ({
 }: CourseCardsProps) => {
     return (
         <Link href={`/courses/${id}`}>
-            <div className="group h-full shadow-sm transition overflow-hidden border rounded-lg p-3">
-                
+            <div className="group h-full hover:shadow-sm transition overflow-hidden border rounded-lg p-3 pb-0 mt-3">
+                <div className="w-full relative aspect-video rounded-md overflow-hidden">
+                    <Image 
+                        alt="course image"
+                        src={imageUrl}
+                        fill
+                        className="object-cover"
+                    />
+                </div>
+                <div className="flex flex-col pt-2">
+                    <div className="text-lg md:text-base font-medium group-hover:text-gray-700 line-clamp-2">
+                        {title}
+                    </div>
+                    <p className="text-xs text-muted-foreground">
+                        {category}
+                    </p>
+                    <div className="flex items-center gap-x-2 my-3 text-sm md:text-xs">
+                        <div className="flex items-center gap-x-1 text-slate-500">
+                            <IconBadge size="sm" icon={BookOpen} />
+                            <span>
+                                {chaptersLength} {chaptersLength === 1 ? "Chapter" : "Chapters"}
+                            </span>
+                        </div>
+                    </div>
+                    {progress !== null ? (
+                        <div>
+                            TODO
+                        </div>
+                    ): (
+                        <p className="text-md md:text-sm font-medium text-slate-700">
+                            {formatPrice(price)}
+                        </p>
+                    )}
+                </div>
             </div>
         </Link>
     )
