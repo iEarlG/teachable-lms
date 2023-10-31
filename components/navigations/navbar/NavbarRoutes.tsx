17b46caa865a9d835daa805712099ba7c7188e67
Earl A. Villapaz <jasonburning16@gmail.com>
2023-10-31 20:27:48 +0800

    34: Teacher Mode Protection

--- components/navigations/navbar/NavbarRoutes.tsx
+++ components/navigations/navbar/NavbarRoutes.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { UserButton, useAuth } from "@clerk/nextjs";
 
 import { LogOut } from "lucide-react";
+import { isTeacher } from "@/lib/teacher";
 
 import { Button } from "@/components/ui/button";
 import { SearchInput } from "@/components/search/SearchInput";
 
 export const NavbarRoutes = () => {
+    const { userId } = useAuth();
     const pathname = usePathname();
 
     const isTeacherPage = pathname?.startsWith("/teacher");
@@ -31,13 +33,13 @@ export const NavbarRoutes = () => {
                             Exit
                         </Button>
                     </Link>
-                ): (
+                ): isTeacher(userId) ? (
                     <Link href="/teacher/courses">
                         <Button size="sm" variant="ghost">
                             Teacher Mode
                         </Button>
                     </Link>
-                )}
+                ): null}
                 <UserButton afterSignOutUrl="/" />
             </div>
         </>
---
+++ lib/teacher.ts
@@ -0,0 +1,8 @@
+
+export const isTeacher = (
+    userId?: string | null
+) => {
+    return (
+        userId === process.env.NEXT_PUBLIC_TEACHER_ID
+    )
+};
